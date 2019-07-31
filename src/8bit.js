714ba0eda79c6731b3f3fb145a7f0cefba547e24
Ariel Saldana <hello@ariel.io>
2019-07-30 20:14:02 -0400

    8bit js

---
+++ src/8bit.js
@@ -0,0 +1,55 @@
+(function(){
+    "use strict"
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // dot
+
+    ctx.fillRect(25, 16, 1, 1);
+
+    ctx.fillRect(24, 15, 1, 1);
+    ctx.fillRect(26, 15, 1, 1);
+
+    ctx.fillRect(23, 14, 1, 1);
+    ctx.fillRect(22, 14, 1, 1);
+    ctx.fillRect(27, 14, 1, 1);
+    ctx.fillRect(28, 14, 1, 1);
+
+    ctx.fillRect(21, 14, 1, 1);
+    ctx.fillRect(20, 15, 1, 1);
+    ctx.fillRect(19, 16, 1, 1);
+
+    ctx.fillRect(29, 14, 1, 1);
+    ctx.fillRect(30, 15, 1, 1);
+    ctx.fillRect(31, 16, 1, 1);
+
+    ctx.fillRect(19, 17, 1, 1);
+    ctx.fillRect(31, 17, 1, 1);
+    ctx.fillRect(19, 18, 1, 1);
+    ctx.fillRect(31, 18, 1, 1);
+
+    ctx.fillRect(20, 19, 1, 1);
+    ctx.fillRect(30, 19, 1, 1);
+
+    ctx.fillRect(21, 20, 1, 1);
+    ctx.fillRect(29, 20, 1, 1);
+
+    ctx.fillRect(22, 21, 1, 1);
+    ctx.fillRect(28, 21, 1, 1);
+
+    ctx.fillRect(23, 22, 1, 1);
+    ctx.fillRect(27, 22, 1, 1);
+
+    ctx.fillRect(24, 23, 1, 1);
+    ctx.fillRect(26, 23, 1, 1);
+
+    ctx.fillRect(25, 24, 1, 1);
+    // ctx.fillRect(25, 20, 1, 1);
+
+
+
+
+    ctx.closePath();
+    ctx.stroke();
+}())
